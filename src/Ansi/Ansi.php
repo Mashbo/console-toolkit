bef868fe2e21b4e1f8781b3ebfb4fe144cde2da1
Adam Quaile <aquaile@mashbo.com>
2016-10-25 15:46:45 +0100

    Added all foreground colours from http://misc.flogisoft.com/bash/tip_colors_and_formatting

--- src/Ansi/Ansi.php
+++ src/Ansi/Ansi.php
@@ -4,6 +4,25 @@ namespace Mashbo\ConsoleToolkit\Ansi;
 
 final class Ansi
 {
+    private static $fgColours = [
+        'black'         => 30,
+        'red'           => 31,
+        'green'         => 32,
+        'yellow'        => 33,
+        'blue'          => 34,
+        'magenta'       => 35,
+        'cyan'          => 36,
+        'lightGray'     => 37,
+        'darkGray'      => 90,
+        'lightRed'      => 91,
+        'lightGreen'    => 92,
+        'lightYellow'   => 93,
+        'lightBlue'     => 94,
+        'lightMagenta'  => 95,
+        'lightCyan'     => 96,
+        'white'         => 97
+    ];
+
     public static function backspace()
     {
         return "\e[1D";
@@ -11,7 +30,43 @@ final class Ansi
 
     public static function green($text)
     {
-        return "\e[32m$text\e[39m";
+        return self::changeForegroundColour($text, self::$fgColours[__FUNCTION__]);
+    }
+
+    public static function white($text)
+    {
+        return self::changeForegroundColour($text, self::$fgColours[__FUNCTION__]);
+    }
+
+
+    public static function red($text)
+    {
+        return self::changeForegroundColour($text, self::$fgColours[__FUNCTION__]);
+    }
+
+    public static function lightRed($text)
+    {
+        return self::changeForegroundColour($text, self::$fgColours[__FUNCTION__]);
+    }
+
+    public static function yellow($text)
+    {
+        return self::changeForegroundColour($text, self::$fgColours[__FUNCTION__]);
+    }
+
+    public static function blue($text)
+    {
+        return self::changeForegroundColour($text, self::$fgColours[__FUNCTION__]);
+    }
+
+    public static function lightBlue($text)
+    {
+        return self::changeForegroundColour($text, self::$fgColours[__FUNCTION__]);
+    }
+
+    public static function lightYellow($text)
+    {
+        return self::changeForegroundColour($text, self::$fgColours[__FUNCTION__]);
     }
 
     public static function cursorUp()
@@ -32,5 +87,15 @@ final class Ansi
     public static function bold($text)
     {
         return "\e[1m$text\e[22m";
+    }
+
+    /**
+     * @param $text
+     * @param $code
+     * @return string
+     */
+    private static function changeForegroundColour($text, $code)
+    {
+        return "\e[{$code}m$text\e[39m";
     }
 }
--- tests/Ansi/AnsiTest.php
+++ tests/Ansi/AnsiTest.php
@@ -7,13 +7,38 @@ use Mashbo\ConsoleToolkit\Ansi\Ansi;
 class AnsiTest extends \PHPUnit_Framework_TestCase
 {
 
-    public function test_text_can_be_wrapped_in_green()
+    /**
+     * @dataProvider colourCodeToMethodNameDataProvider
+     */
+    public function test_text_can_be_wrapped_in_colour($code, $methodName)
     {
-        $this->assertEquals(chr(27)."[32mTest text" . chr(27)."[39m", Ansi::green('Test text'));
+        $this->assertEquals(chr(27)."[{$code}mTest text" . chr(27)."[39m", Ansi::$methodName('Test text'));
     }
 
     public function test_backspace_is_returned()
     {
         $this->assertEquals(chr(27)."[1D", Ansi::backspace());
+    }
+
+    public function colourCodeToMethodNameDataProvider()
+    {
+        return [
+            [30, 'black'],
+            [31, 'red'],
+            [32, 'green'],
+            [33, 'yellow'],
+            [34, 'blue'],
+            [35, 'magenta'],
+            [36, 'cyan'],
+            [37, 'lightGray'],
+            [90, 'darkGray'],
+            [91, 'lightRed'],
+            [92, 'lightGreen'],
+            [93, 'lightYellow'],
+            [94, 'lightBlue'],
+            [95, 'lightMagenta'],
+            [96, 'lightCyan'],
+            [97, 'white'],
+        ];
     }
 }
