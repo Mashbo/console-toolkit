aa7c846489efb14e6b7090f5bf4f416f60e1e45e
Adam Quaile <adamquaile@gmail.com>
2016-10-25 16:09:41 +0100

    Added all foreground colours from http://misc.flogisoft.com/bash/tip_… (#3)
    Added all foreground colours from http://misc.flogisoft.com/bash/tip_colors_and_formatting

--- src/Ansi/Ansi.php
+++ src/Ansi/Ansi.php
@@ -4,6 +4,25 @@ namespace Mashbo\ConsoleToolkit\Ansi;
 
 final class Ansi
 {
+    private static $fgColours = [
+        'black'         => 30,
+        'red'           => 31,
+        'green'         => 32,
+        'yellow'        => 33,
+        'blue'          => 34,
+        'magenta'       => 35,
+        'cyan'          => 36,
+        'lightGray'     => 37,
+        'darkGray'      => 90,
+        'lightRed'      => 91,
+        'lightGreen'    => 92,
+        'lightYellow'   => 93,
+        'lightBlue'     => 94,
+        'lightMagenta'  => 95,
+        'lightCyan'     => 96,
+        'white'         => 97
+    ];
+
     public static function backspace()
     {
         return "\e[1D";
@@ -11,7 +30,82 @@ final class Ansi
 
     public static function green($text)
     {
-        return "\e[32m$text\e[39m";
+        return self::changeForegroundColour($text, self::$fgColours[__FUNCTION__]);
+    }
+
+    public static function white($text)
+    {
+        return self::changeForegroundColour($text, self::$fgColours[__FUNCTION__]);
+    }
+
+    public static function black($text)
+    {
+        return self::changeForegroundColour($text, self::$fgColours[__FUNCTION__]);
+    }
+
+    public static function magenta($text)
+    {
+        return self::changeForegroundColour($text, self::$fgColours[__FUNCTION__]);
+    }
+
+    public static function cyan($text)
+    {
+        return self::changeForegroundColour($text, self::$fgColours[__FUNCTION__]);
+    }
+
+    public static function lightGray($text)
+    {
+        return self::changeForegroundColour($text, self::$fgColours[__FUNCTION__]);
+    }
+
+    public static function lightGreen($text)
+    {
+        return self::changeForegroundColour($text, self::$fgColours[__FUNCTION__]);
+    }
+
+    public static function lightMagenta($text)
+    {
+        return self::changeForegroundColour($text, self::$fgColours[__FUNCTION__]);
+    }
+
+    public static function lightCyan($text)
+    {
+        return self::changeForegroundColour($text, self::$fgColours[__FUNCTION__]);
+    }
+
+    public static function darkGray($text)
+    {
+        return self::changeForegroundColour($text, self::$fgColours[__FUNCTION__]);
+    }
+
+    public static function red($text)
+    {
+        return self::changeForegroundColour($text, self::$fgColours[__FUNCTION__]);
+    }
+
+    public static function lightRed($text)
+    {
+        return self::changeForegroundColour($text, self::$fgColours[__FUNCTION__]);
+    }
+
+    public static function yellow($text)
+    {
+        return self::changeForegroundColour($text, self::$fgColours[__FUNCTION__]);
+    }
+
+    public static function blue($text)
+    {
+        return self::changeForegroundColour($text, self::$fgColours[__FUNCTION__]);
+    }
+
+    public static function lightBlue($text)
+    {
+        return self::changeForegroundColour($text, self::$fgColours[__FUNCTION__]);
+    }
+
+    public static function lightYellow($text)
+    {
+        return self::changeForegroundColour($text, self::$fgColours[__FUNCTION__]);
     }
 
     public static function cursorUp()
@@ -32,5 +126,15 @@ final class Ansi
     public static function bold($text)
     {
         return "\e[1m$text\e[22m";
+    }
+
+    /**
+     * @param $text
+     * @param $code
+     * @return string
+     */
+    private static function changeForegroundColour($text, $code)
+    {
+        return "\e[{$code}m$text\e[39m";
     }
 }
